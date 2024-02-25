cffa5a1153efa5832002b31a3729fa23c58a4ad9
Norukung <138838596+Norukung@users.noreply.github.com>
2024-02-25 15:52:44 +0700

    Add files via upload

---
+++ work8.py
@@ -0,0 +1,26 @@
+# ข้อ1
+n = 1
+while n <= 21:
+        print(n , end= ", " )
+        n += 2
+# ข้อ 2
+n = 2      
+while n <= 17:
+        print(n , end= ", " )
+        n += 3
+# ข้อ 3
+n = 100
+while n >= -100:
+        print(n , end= ", " )
+        n -= 10
+# ข้อ 4
+n = -30
+while n <= 30:
+        print(n , end= ", " )
+        n += 10
+# ข้อ 5
+n = 15
+while n <= 55:
+        print(n , end= ", " )
+        n += 8
+        
